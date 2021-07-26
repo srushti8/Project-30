15c37424367931a909f436de9cdc16204279bccd
srushti8 <76839929+srushti8@users.noreply.github.com>
2021-07-26 16:46:57 +0530

    Add files via upload

--- Block.js
+++ Block.js
@@ -13,8 +13,14 @@ class Block{
     }
     display(){
       if (this.body.speed < 3){
-       //super.display();
-     }
+        var angle = this.body.angle; 
+        var pos= this.body.position; 
+        push(); 
+        translate(pos.x, pos.y); 
+        rotate(angle); rectMode(CENTER); 
+        rect(0,0,this.width, this.height); 
+        pop();
+      }
      else{
        World.remove(world,this.body);
        push();
@@ -22,10 +28,7 @@ class Block{
        tint(255,this.visibility);
        image(this.image,this.body.position.x,this.body.position.y,50,50);
        pop();
-     }
+      }
      
     }
-     //var pos= this.body.position;
-     // rectMode(CENTER);
-     // rect(pos.x,pos.y,this.width, this.height);
 }
